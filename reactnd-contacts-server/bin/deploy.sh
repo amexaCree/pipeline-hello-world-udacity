f6b6c1ac13eccb5467b61c4bb57cf1ea584a8817
Amy Turnah <amy@scarlettmadz.com>
2021-10-24 18:12:04 +1100

    fix typo in package.json + update server deploy script

--- reactnd-contacts-server/bin/deploy.sh
+++ reactnd-contacts-server/bin/deploy.sh
@@ -1,3 +1,10 @@
+
 # npm script: chmod +x ./bin/deploy.sh
 
-eb deploy
+# eb use test5 --region us-east-1
+
+# uses .elasticbeanstalk/config.yml file for settings
+eb deploy test5
+
+# check status of application
+eb status
