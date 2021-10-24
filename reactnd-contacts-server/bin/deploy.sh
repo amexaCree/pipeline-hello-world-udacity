526e2f5615e3f51bcd548ef70391e8752e95f775
Amy Turnah <amy@scarlettmadz.com>
2021-10-24 18:37:35 +1100

    add more changes

--- reactnd-contacts-server/bin/deploy.sh
+++ reactnd-contacts-server/bin/deploy.sh
@@ -1,10 +1,11 @@
 
-# npm script: chmod +x ./bin/deploy.sh
+# npm script: 
+#  chmod +x ./bin/deploy.sh && ./bin/deploy.sh
 
 # eb use test5 --region us-east-1
 
 # uses .elasticbeanstalk/config.yml file for settings
-eb deploy test5
+eb deploy
 
 # check status of application
 eb status
